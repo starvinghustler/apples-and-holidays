a606e33db0923b842c6e57f29bee87ca962a2ee9
idobok <idobok12@gmail.com>
2021-11-16 15:37:30 +0200

    commit tree 3d2a36490983738a333e9ab9df46fbace8ca96dc

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,8 +47,8 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 holiday_hash[:winter].map do |holiday, supplies|
-  supplies.flatten
-end
+  supplies
+end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -65,7 +65,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_hash.map do |season, holidays|
+  holidays.map do |holiday, supplies|
+    holiday if supplies.include?("BBQ")
+  end
+end.flatten.compact
 end
 
 
